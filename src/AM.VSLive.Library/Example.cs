48b41e4428e590b6c74a4c2bde391bffc78be0e6
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2017-03-15 07:27:59 -0700

    feat(divide): add support for dividing two values (#1)
    * chore: add git config
    
    * chore: add stylecop config
    
    * chore: add dotnet core solution
    
    * chore: add editorconfig
    
    * chore: add vscode config
    
    * feat(divide): add support for dividing two values

---
+++ src/AM.VSLive.Library/Example.cs
@@ -0,0 +1,25 @@
+﻿namespace AM.VSLive.Library
+{
+    /// <summary>
+    /// Represents an example.
+    /// </summary>
+    public class Example
+    {
+        /// <summary>
+        /// Divides the left operand by the right operand.
+        /// </summary>
+        /// <param name="left">
+        /// The left operand of the divide operation.
+        /// </param>
+        /// <param name="right">
+        /// The right operand of the divide operation.
+        /// </param>
+        /// <returns>
+        /// The result of the operation.
+        /// </returns>
+        public float Divide(int left, int right)
+        {
+            return left / right;
+        }
+    }
+}
